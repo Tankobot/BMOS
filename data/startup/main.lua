1f576ec8589a1b46ec3487de8a25a5f8f63378b8
Tankobot <Tankobot@noreply.github.com>
2015-03-27 12:25:51 -0700

    Add Comfirmation Screen.

--- data/startup/main.lua
+++ data/startup/main.lua
@@ -2,5 +2,33 @@
 Title: Startup 
 --]]
 
-dofile("lib")
+local lib = dofile("lib/lib.lua")
 lib.load("tskMng.lua")
+lib.load("gui.lua")
+
+local center 
+do 
+	local x, y = term.getSize()
+	center = {math.floor(x/2), math.floor(y/2)}
+	if x%2 then 
+		center.x = true
+	end
+	if y%2 then 
+		center.y = true
+	end
+end
+
+local scp = term.setCursosPos
+term.clear()
+scp(center[1], center[2])
+
+gui.write("Start?\nTerminate to exit.")
+local event = os.pullEventRaw()
+if event == "terminate" then 
+	os.shutdown()
+else
+	term.clear()
+	scp(1,1)
+	print("Starting BMOS...")
+	sleep(0.1)
+end
