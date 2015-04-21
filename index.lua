9d16f41b3036c1194eb426fc09e08bdb5b6748fd
Tankobot <Tankobot@noreply.github.com>
2015-04-21 13:39:14 -0700

    Finish index of BMOS file.
    Updated as of the time of this commit.

---
+++ index.lua
@@ -0,0 +1,39 @@
+--File index 
+
+local arg = {...}
+if arg[2] then 
+	arg[2] = "/tree/v"
+else
+	arg[2] = ""
+end
+local PATH = "Tankobot/BMOS/"..arg[1]..arg[2]
+
+local BMOS = {
+	data = {
+		startup = {
+			"main.lua",
+		},
+	},
+	lib = {
+		os = {
+			"argSorter.lua",
+			"codec.lua",
+			"gui.lua",
+			"net.lua",
+			"task.lua",
+		},
+	},
+	tools = {
+		"CCIedit.lua",
+		"login.lua",
+		"startup",
+		"sudo.lua",
+		"taskManager",
+		"trampoline.lua",
+	},
+	"index.lua",
+	"install.lua",
+	"README.md",
+}
+
+return BMOS, PATH
