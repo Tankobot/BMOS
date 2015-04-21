fe285c21f7aa52c1f353d6531d2f0b1edafb12cc
Tankobot <Tankobot@noreply.github.com>
2015-04-21 07:35:40 -0700

    Delete unneccasary code.

--- data/startup/main.lua
+++ data/startup/main.lua
@@ -3,21 +3,4 @@ Title: Startup
 --]]
 
 local lib = dofile("lib/lib.lua")
-lib.load("tskMng.lua")
-lib.load("gui.lua")
-
-local center 
-do 
-	local x, y = term.getSize()
-	center = {math.floor(x/2), math.floor(y/2)}
-	if x%2 then 
-		center.x = true
-	end
-	if y%2 then 
-		center.y = true
-	end
-end
-
-local scp = term.setCursosPos
-term.clear()
-scp(center[1], center[2])
+local task = lib.load("task.lua")
