c46fc4439534f245666500fcb8594977e652011e
Tankobot <Tankobot@noreply.github.com>
2015-04-22 13:25:03 -0700

    Update and index are still in a non-working condition.
    * Progress has been made!

--- index.lua
+++ index.lua
@@ -1,13 +1,5 @@
 --File index 
 
-local arg = {...}
-if arg[2] then 
-	arg[2] = "/tree/"..arg[2]
-else
-	arg[2] = ""
-end
-local PATH = "Tankobot/BMOS/"..arg[1]..arg[2]
-
 local BMOS = {
 	data = {
 		startup = {
@@ -34,6 +26,7 @@ local BMOS = {
 	"index.lua",
 	"README.md",
 	"update.lua",
+	"end",
 }
 
 return BMOS, PATH
--- update.lua
+++ update.lua
@@ -2,3 +2,23 @@
 Title: BMOS Updater
 Version: v0.1
 --]] 
+
+local _NAME = "Tankobot/BMOS/"
+local _PATH = "https://raw.githubusercontent.com/".._NAME
+local download
+local arg = {...}
+
+assert(http, "The updater requires the 'http' api in order to download necessary files.")
+
+local function download(dirTable, currentPath)
+	for i, v in pairs(dirTable) do
+		if type(v) == "table" then
+			local status, message = download(v, currentPath..v.."/"
+		elseif type(v) == "string" then
+			http.request("https://raw.githubusercontent.com/"..
+			"Tankobot/BMOS/"..arg[1]..currentPath)
+		else
+			error("Index formatted incorrectly, expected 'table' or 'string'. Got "..type(v), 0)
+		end
+	end
+end
