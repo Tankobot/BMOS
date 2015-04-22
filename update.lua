b6190f2ee661c1781729294a505c8af16286bfad
Tankobot <Tankobot@noreply.github.com>
2015-04-22 14:28:35 -0700

    Update update.lua.
    * Functions are almost finished.
    * http2drive needs to be completed.
    * Not tested.
    
    Signed-off-by: Tankobot <Tankobot@noreply.github.com>

--- update.lua
+++ update.lua
@@ -7,18 +7,41 @@ local _NAME = "Tankobot/BMOS/"
 local _PATH = "https://raw.githubusercontent.com/".._NAME
 local download
 local arg = {...}
+local place
+local names = {}
 
 assert(http, "The updater requires the 'http' api in order to download necessary files.")
 
-local function download(dirTable, currentPath)
+local function download(dirTable, currentPath, statName)
 	for i, v in pairs(dirTable) do
 		if type(v) == "table" then
-			local status, message = download(v, currentPath..v.."/"
+			local status, message = pcall(download, v, currentPath..i.."/")
+			if not status then
+				error(message, 0)
+			else
+				return message
+			end
 		elseif type(v) == "string" then
-			http.request("https://raw.githubusercontent.com/"..
-			"Tankobot/BMOS/"..arg[1]..currentPath)
+			if v == "end" then
+				return true
+			else
+				place = _PATH..arg[1]..currentPath
+				http.request(place)
+				names[place] = currentPath..v
+			end
 		else
 			error("Index formatted incorrectly, expected 'table' or 'string'. Got "..type(v), 0)
+		end
+	end
+end
+
+local function http2drive(path, names)
+	while true do
+		local event = {os.pullEvent()}
+		if event[1] == "http_success" then
+			--TODO
+		elseif event[1] == "http_failure" then
+			print("Failed to download file at '"..names[event[2]].."'.")
 		end
 	end
 end
