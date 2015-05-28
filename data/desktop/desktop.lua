a9bc80b2690229ee7d816777ff5051565b3c2eb3
Tankobot <Tankobot@users.noreply.github.com>
2015-05-28 07:48:46 -0700

    Incomplete include main loop in desktop.

--- data/desktop/desktop.lua
+++ data/desktop/desktop.lua
@@ -1,4 +1,4 @@
---Desktop Script 
+--Desktop Script
 assert(lib, "Lib library not present!")
 assert(bmos, "BMOS library not present!")
 
@@ -8,14 +8,19 @@ local x, y = term.getSize()
 local cx, cy = gui.center()
 local bg, tx, fg = unpack(bmos.style.desktop)
 
-local backg = {set = gui.createSet()} --Setup layer 
+local backg = {set = gui.createSet()} --Setup layer
 do
 	local layer = backg.set
-	local desktop = layer.add(1, 1, x, y-1)
-	local actionBar = layer.add(1, y, x, 1)
-	
+	local desktop = layer.add(1, 1, x, y-1) --Background
+	local actionBar = layer.add(1, y, x, 1) --Home Button
+
 	layer:set(desktop, bg, tx, bg)
 	layer:set(actionBar, fg, tx, fg)
-	
+
 	layer:setText(actionBar, ("%"):rep(x))
 end
+
+--[[while true do --Main Loop
+	local event = {os.pullEvent()}
+	if event[1]==
+end--]]
