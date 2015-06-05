b2d58c3dbafba93e8a6355cca51b6d9bf0155b59
Tankobot <Tankobot@users.noreply.github.com>
2015-06-04 22:50:14 -0700

    Add kernal host file.

---
+++ kernal/host.lua
@@ -0,0 +1,18 @@
+--[[
+  _  __                     _ 
+ | |/ /                    | |
+ | ' / ___ _ __ _ __   __ _| |
+ |  < / _ \ '__| '_ \ / _` | |
+ | . \  __/ |  | | | | (_| | |
+ |_|\_\___|_|  |_| |_|\__,_|_|
+
+ASCII art generated with: 
+http://patorjk.com/software/taag/
+--]]
+
+--Main Loop 
+while true do
+	--TODO Kernal
+end
+
+return nil
