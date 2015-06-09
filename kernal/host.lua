5b9cdfaad2407c60cc11ae4e16238dfc137a7647
Tankobot <Tankobot@users.noreply.github.com>
2015-06-08 23:27:58 -0700

    Add the building blocks of the kernal.
    Still unfinished. Only skeleton of libraries in place.

--- kernal/host.lua
+++ kernal/host.lua
@@ -10,6 +10,42 @@ ASCII art generated with:
 http://patorjk.com/software/taag/
 --]]
 
+--Load Task Manager Library 
+local task = dofile("lib/os/task.la")
+
+local driver = {
+	nil
+}
+
+--Setup Kernal Library 
+local kernal = {}
+
+function kernal.inject(thread, var)
+	
+end
+
+function kernal.kill(thread)
+	
+end
+
+function kernal.requestPermission(priv, sms)
+	
+end
+--==
+
+--I/O Library
+local nPer = peripheral
+local peripheral = {}
+
+function peripheral.wrap(side)
+	
+end
+
+function peripheral.call(side, command, ...)
+	
+end
+--==
+
 --Main Loop 
 while true do
 	--TODO Kernal
