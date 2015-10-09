09fc2f72d740b6c6797cb7be4348ed65fddd6196
Michael B. <Tankobot@users.noreply.github.com>
2015-10-09 07:43:24 -0700

    Create corunner.lua

---
+++ system/corunner.lua
@@ -0,0 +1,7 @@
+--This is the coroutine execution program for bmos. 
+--[[Rundown(Psuedocode)
+Initialize permissions table
+Initialize coroutine table
+Define corunner commands 
+Load main coroutine with admin permissions 
+--]]Unfinished
